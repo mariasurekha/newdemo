9d55367e168c69150ba7c75e2a1022e3034d26eb
Training_C2A.04.29 <Training_C2A.04.29@cdc2-d-46F8N62.dir.svc.accenture.com>
2019-03-29 18:13:18 +0530

    commit

---
+++ training selenium/src/Employee.java
@@ -0,0 +1,16 @@
+
+public class Employee {
+	public int empid1;
+	String empname;
+	int empphn;
+//	public int empid1;
+
+	public void displaydetails()
+	{
+	System.out.println(empid1+"\t"+empname+"\t"+empphn);
+	}
+	public Employee()
+	{
+		System.out.println("the data is initialized");
+	}
+}
---
+++ training selenium/src/RunValidation1.java
@@ -0,0 +1,14 @@
+
+public class RunValidation1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Employee emp1= new Employee();
+		emp1.empid1=01;
+		emp1.empname="Aaa";
+		emp1.empphn=1234567890;
+		
+ emp1.displaydetails();
+	}
+
+}
